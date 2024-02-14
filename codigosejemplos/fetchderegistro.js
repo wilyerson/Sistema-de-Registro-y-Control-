216a18d193a6c79c8608bd82568b57048580be19
optimusprain <112661750+optimusprain@users.noreply.github.com>
2024-02-14 08:52:04 -0400

    Add files via upload

---
+++ codigosejemplos/fetchderegistro.js
@@ -0,0 +1,24 @@
+const formdata = document.querySelector("#formdata");
+formdata.addEventListener("submit", (e) => {
+	e.preventDefault();
+	const datos = new FormData(document.getElementById("formdata"));
+
+	console.log(" conectado");
+
+	let url = "../../controller/controller.php?op=guardar";
+	fetch(url, {
+		method: "post",
+		body: datos,
+	})
+		.then((data) => data.json())
+		.then((data) => {
+			//console.log(`Success: ${JSON.stringify(data)}`);
+			dibujarTabla(data);
+			formdata.reset();
+			swal.fire({
+				title: "¡Registro Exitoso!",
+				icon: "success",
+			});
+		})
+		.catch((error) => console.log(`error: ${error}`));
+});
